d75d8d66f613d8c618fa2baf108261362c9a75ab
Norton Allen <allen@huarp.harvard.edu>
2021-10-22 19:54:51 -0400

    Remove pyqt4 dependence. Whitespace fixes

--- simulation.py
+++ simulation.py
@@ -5,7 +5,7 @@
 # First, and before importing any Enthought packages, set the ETS_TOOLKIT
 # environment variable to qt4, to tell Traits that we will use Qt.
 import os
-os.environ['ETS_TOOLKIT'] = 'qt4'
+# os.environ['ETS_TOOLKIT'] = 'qt4'
 # By default, the PySide binding will be used. If you want the PyQt bindings
 # to be used, you need to set the QT_API environment variable to 'pyqt'
 #os.environ['QT_API'] = 'pyqt'
@@ -56,7 +56,7 @@ class Visualization(HasTraits):
         # This function is called when the view is opened. We don't
         # populate the scene when the view is not yet open, as some
         # VTK features require a GLContext.
-        
+
         # Nothing works, nothing ever works, what the fuck
         #fig=mlab.gcf()
         #fig=mlab.figure(bgcolor=(0.52,0.8,0.92))
@@ -68,10 +68,10 @@ class Visualization(HasTraits):
         # the cloud and dots don't show up because the resolution is off
         #earth=mlab.points3d(0,0,-6030000,mode='sphere',scale_factor=6000000,color=(0,0.25,0))
         #earth_horizon = mlab.barchart(0,0,-30000,mode='2dcircle', lateral_scale=200,scale_factor=30000, color=(0,0.25,0),reset_zoom=False)
-        
+
         # For some reason this is the line that causes that annoying error message:
         #mlab.xlabel("origin")
-        
+
         #s = np.random.random((1000, 1000))
         #earth=mlab.imshow(s,colormap='gist_earth')
 
@@ -89,7 +89,7 @@ class Visualization(HasTraits):
 
         # We can do normal mlab calls on the embedded scene.
         #self.scene.mlab.test_points3d()
-        
+
     # the layout of the dialog screated
     view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene),
                      height=250, width=300, show_label=False),
@@ -114,7 +114,7 @@ class MayaviQWidget(QtGui.QWidget):
 
         # The edit_traits call will generate the widget to embed.
         self.ui = self.visualization.edit_traits(parent=self,kind='subpanel').control
-        
+
         layout.addWidget(self.ui)
         self.ui.setParent(self)
 
@@ -137,7 +137,7 @@ if __name__ == "__main__":
     vtk.vtkObject.GlobalWarningDisplayOff()
     #renderer=vtk.vtkRenderer()
     #renderer.SetBackground
-    
+
     # Don't create a new QApplication, it would unhook the Events
     # set by Traits on the existing QApplication. Simply use the
     # '.instance()' method to retrieve the existing one.
@@ -157,13 +157,13 @@ if __name__ == "__main__":
             # layout.addWidget(label, i, j)
             # label_list.append(label)
     mayavi_widget = MayaviQWidget(container)
-    
+
     new_gondola = gondola.Gondola((0,0,0),wait=latency,c_l=command_latency,max_size=maxsize,r_s=rotation,res=resolution,s_a=shear_azimuth,s_m=shear_magnitude)
     new_gondola.lidar.gondola=new_gondola
     #new_gondola.new_cloud.gondola=new_gondola
     command_queue=interactions.Command_Queue(new_gondola,rel=reliability)
     new_gondola.command_queue=command_queue
-    
+
     layout_2 = QtGui.QGridLayout()
     layout_display = QtGui.QGridLayout()
     layout_display_azimuth = QtGui.QGridLayout()
@@ -175,45 +175,45 @@ if __name__ == "__main__":
     speed_slider.connect_value_changed()
     angle_slider = interactions.AngleSlider(new_gondola,command_queue)
     angle_slider.connect_value_changed()
-    
+
     button_l = interactions.DirectionButton("Turn Left",-1,new_gondola,command_queue)
     button_r = interactions.DirectionButton("Turn Right",1,new_gondola,command_queue)
     button_l.connect_released()
     button_r.connect_released()
-    
+
     lidar_button_l=interactions.LidarScanDirectionButton("LIDAR Scan Left",-1,new_gondola,command_queue)
     lidar_button_r=interactions.LidarScanDirectionButton("LIDAR Scan Right",1,new_gondola,command_queue)
     lidar_button_l.connect_released()
     lidar_button_r.connect_released()
-    
+
     layout_2.addWidget(button_l,0,0)
     layout_2.addWidget(lidar_button_l,0,1)
     layout_2.addWidget(lidar_button_r,0,2)
     layout_2.addWidget(button_r,0,3)
-    
+
     timer=QtCore.QTimer()
     timer.timeout.connect(new_gondola.default)
     timer.start(1000)
-    
+
     pause_button=interactions.PauseButton("Pause",new_gondola)
     pause_button.connect_released()
     cloud_button=interactions.CloudButton("Cloud OFF",new_gondola)
     cloud_button.connect_released()
     cloud_spray_button=interactions.CloudSprayOff("Terminate Cloud Spray",new_gondola)
     cloud_spray_button.connect_released()
-    
+
     combo_box = QtGui.QComboBox()
     combo_box.addItem("LIDAR OFF")
     combo_box.addItem("LIDAR Multi Scan")
     combo_box.addItem("LIDAR Horizontal Scan")
     combo_box.addItem("LIDAR Vertical Scan")
     combo_box.activated[str].connect(new_gondola.mode_select)
-    
+
     layout_speed_slider=QtGui.QGridLayout()
     layout_speed_labels=QtGui.QGridLayout()
     layout_angle_slider=QtGui.QGridLayout()
     layout_angle_labels=QtGui.QGridLayout()
-    
+
     speed_slider_name=QtGui.QLabel()
     speed_slider_name.setText("Gondola Movement Speed")
     speed_slider_name.setAlignment(QtCore.Qt.AlignCenter)
@@ -230,21 +230,21 @@ if __name__ == "__main__":
     angle_slider_max=QtGui.QLabel()
     angle_slider_max.setText("45")
     angle_slider_max.setAlignment(QtCore.Qt.AlignRight)
-    
+
     layout_speed_labels.addWidget(speed_slider_min,0,0)
     layout_speed_labels.addWidget(speed_slider_name,0,1)
     layout_speed_labels.addWidget(speed_slider_max,0,2)
-    
+
     layout_angle_labels.addWidget(angle_slider_min,0,0)
     layout_angle_labels.addWidget(angle_slider_name,0,1)
     layout_angle_labels.addWidget(angle_slider_max,0,2)
-    
+
     layout_speed_slider.addLayout(layout_speed_labels,0,0)
     layout_speed_slider.addWidget(speed_slider,1,0)
-    
+
     layout_angle_slider.addLayout(layout_angle_labels,0,0)
     layout_angle_slider.addWidget(angle_slider,1,0)
-    
+
     az1_display=QtGui.QLabel()
     az2_display=QtGui.QLabel()
     display_speed=QtGui.QLabel()
@@ -255,15 +255,15 @@ if __name__ == "__main__":
     display_angle.setStyleSheet("color: green")
     position1_display=QtGui.QLabel()
     position2_display=QtGui.QLabel()
-    
+
     az1_display.setText("0")
     az2_display.setText("0")
-    
+
     myFont=QtGui.QFont()
     myFont.setBold(True)
     display_speed.setFont(myFont)
     display_angle.setFont(myFont)
-    
+
     display_speed.setText("(3)")
     display_angle.setText("(23)")
     #print(display_speed.width())
@@ -276,33 +276,33 @@ if __name__ == "__main__":
     #display_angle.setGeometry(QtCore.QRect(display_angle.x(),display_angle.y(),display_angle.width()+500,display_angle.height()))
     position1_display.setText("0")
     position2_display.setText("0")
-    
+
     speed_slider.speed_label=display_speed
     angle_slider.angle_label=display_angle
-    
+
     layout_4.addWidget(display_speed,0,0)
     layout_4.addLayout(layout_speed_slider,0,1)
     layout_4.addWidget(display_angle,0,2)
     layout_4.addLayout(layout_angle_slider,0,3)
-    
+
     layout_5.addWidget(pause_button,0,0)
     layout_5.addWidget(cloud_button,0,1)
     layout_5.addWidget(cloud_spray_button,0,2)
-    
+
     #view_button=interactions.ViewButton("Distance", gondola)
     #view_button.connect_released()
-    
+
     layout.addLayout(layout_2,1,0)
     layout.addLayout(layout_4,2,0)
     layout.addLayout(layout_5,3,0)
     layout.addWidget(combo_box,4,0)
-    
+
     graph_button=interactions.LidarGraphButton("Graph LIDAR Data",new_gondola)
     graph_button.connect_released()
     layout.addWidget(graph_button,5,0)
-    
+
     #layout.addWidget(view_button,6,0)
-    
+
     container.show()
     window = QtGui.QMainWindow()
     window.setCentralWidget(container)
